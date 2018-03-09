64a8a85e5e9854118b0f7c17c12a4f334447adfa
Ana Catarina <catarinaeliza@live.com>
2018-03-09 15:10:00 -0300

    Primeiro commit

---
+++ src/fundamentos/Comentario.java
@@ -0,0 +1,24 @@
+package fundamentos;
+
+/**
+ * Classe responsável por mostrar o uso dos três tipos de comentários que o Java suporta.
+ * @author Aninha [catarinaeliza@live.com] 
+ *@version 1.0
+ *@since 2.2 
+ */
+public class Comentario {
+	
+	/**
+	 * Esse método é a porta de entrada de um programa Java.
+	 * 
+	 * @param args  parâmetros passados por linha de comando
+	 */
+	public static void main(String[] args) {
+		System.out.println("Primeiro");
+		/*
+		 * Imprime no console a palavra 'Primeiro' e em seguida cria uma nova linha
+		 */
+		// Cuidado como os comentários óbvios
+		System.out.println("Cursinho! <3");
+	}
+}
